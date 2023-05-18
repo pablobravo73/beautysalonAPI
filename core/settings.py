e064a988cfde7a304f74918b3084233e053be14f
pablobravo73 <pablobravo73>
2023-05-17 22:16:49 -0600

    debug

--- core/settings.py
+++ core/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-debug = config('DEBUG', default=False, cast=bool)
-secret_key = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
